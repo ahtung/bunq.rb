11d1993d603da460efae48aa2de99768ab5fd537
Dunya Kirkali <dunyakirkali@gmail.com>
2017-03-14 23:20:14 +0100

    Add session token to configuration

--- lib/bunq_rb/configuration.rb
+++ lib/bunq_rb/configuration.rb
@@ -2,10 +2,12 @@ module BunqRb
   class Configuration
     attr_accessor :api_key
     attr_accessor :key
+    attr_accessor :session_token
 
     def initialize
       @api_key = ''
       @key = nil
+      @session_token = nil
     end
   end
 end
