d4d63209b24b305269d21b54523ace65e781dc59
Dunya Kirkali <dunyakirkali@gmail.com>
2017-03-14 17:01:10 +0100

    Implement initializers for device-server and session-server

--- lib/bunq_rb/objects/device_server.rb
+++ lib/bunq_rb/objects/device_server.rb
@@ -2,9 +2,15 @@ module BunqRb
   class DeviceServer
     URI = '/v1/device-server'
 
+    def initialize(hsh = {})
+      @id = hsh["id"]
+    end
+
     def self.create(hash = {})
       faraday_response = Client.connection.post(URI, hash)
-      json_response = JSON.parse(faraday_response.body) # TODO: (dunyakirkali) error handling
+      json_response = JSON.parse(faraday_response.body)
+      raise json_response["Error"].first["error_description"] if json_response.key?("Error")
+      new(json_response["Response"]["Id"])
     end
   end
 end
--- lib/bunq_rb/objects/session_server.rb
+++ lib/bunq_rb/objects/session_server.rb
@@ -2,9 +2,15 @@ module BunqRb
   class SessionServer
     URI = '/v1/session-server'
 
+    def initialize(hsh = {})
+      @id = hsh["id"]
+    end
+
     def self.create(hash = {})
       faraday_response = Client.connection.post(URI, hash)
       json_response = JSON.parse(faraday_response.body) # TODO: (dunyakirkali) error handling
+      raise json_response["Error"].first["error_description"] if json_response.key?("Error")
+      new(json_response["Response"]['Id'])
     end
   end
 end
