0b34d32a007d4fc242adcedccb08dd4616a6ca14
Dunya Kirkali <dunyakirkali@gmail.com>
2017-03-17 14:11:05 +0100

    Implement first 3 steps of the 3rd scenario

---
+++ spec/scenarios/creating_a_tab_payment_spec.rb
@@ -0,0 +1,53 @@
+require "spec_helper"
+
+RSpec.describe "Scenario" do
+  let(:key) { OpenSSL::PKey::RSA.new 2048 }
+
+  before :each do
+    BunqRb.configure do |config|
+      config.api_key = "c08bbdb62e1d1795ae7e933bc833452fda9c317b4b9d717baeabbc17f8190df9"
+      config.key = key
+    end
+  end
+
+  fit "Create a Tab Payment" do
+    VCR.turned_off do
+      WebMock.allow_net_connect!
+
+      # 1. POST installation
+      _installation, token, _server_public_key = BunqRb::Installation.create(
+        client_public_key: BunqRb.configuration.key.public_key
+      )
+
+      # Set session_token
+      BunqRb.configuration.session_token = token["token"]
+
+      # 2. POST device-server
+      _device_server = BunqRb::DeviceServer.create(
+        description: "Dunya",
+        secret: BunqRb.configuration.api_key,
+        permitted_ips: []
+      )
+
+      # 3. POST session-server
+      _session_server, @token, _user_company = BunqRb::SessionServer.create(
+        secret: BunqRb.configuration.api_key
+      )
+
+      # 4. POST attachment-public
+      image_path = File.expand_path(File.join(File.dirname(__FILE__), "../fixtures/images/baz.jpg"))
+      image = Faraday::UploadIO.new(image_path, 'image/jpeg')
+      attachment = BunqRb::AttachmentPublic.create(image)
+
+      # 5. POST avatar
+      avatar = BunqRb::Avatar.create(
+        attachment_public_uuid: "d93e07e3-d420-45e5-8684-fc0c09a63686"
+      )
+
+      # 6. LIST monetary-account
+      puts BunqRb::MonetaryAccount.all.inspect
+
+      expect(nil).not_to be_nil
+    end
+  end
+end
