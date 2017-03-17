70f3735fe0e1b2e5f8aeba300b3dd1cd3b7450b4
Dunya Kirkali <dunyakirkali@gmail.com>
2017-03-17 23:19:42 +0100

    Correctly implement list permitted-ips

--- spec/bunq_rb/objects/permitted_ip_spec.rb
+++ spec/bunq_rb/objects/permitted_ip_spec.rb
@@ -1,10 +1,11 @@
 require "spec_helper"
 
-RSpec.describe BunqRb::PermittedIp do
-  describe "GET /v1/user/:user_id/credential-password-ip/:credential_password_id/ip", active_session: true do
-    fit "returns an array of :id's" do
-      user_id = 1913
-      credential_password_id = 12
+RSpec.describe BunqRb::PermittedIp, active_session: true do
+  let(:user_id) { 1913 }
+  let(:credential_password_id) { 3088 }
+
+  describe "GET /v1/user/:user_id/credential-password-ip/:credential_password_id/ip" do
+    it "returns an array of :id's" do
       VCR.use_cassette "v1/list_permitted_ips" do
         permitted_ips = described_class.all(user_id, credential_password_id)
         expect(permitted_ips.count).to be(2)
