b570888472c37224cc99215d801fbb0d05cca09b
Dunya Kirkali <dunyakirkali@gmail.com>
2017-03-18 21:52:18 +0100

    Increase coverage

--- spec/bunq_rb/objects/monetary_account_spec.rb
+++ spec/bunq_rb/objects/monetary_account_spec.rb
@@ -3,4 +3,14 @@ require "spec_helper"
 RSpec.describe BunqRb::MonetaryAccount, active_session: true do
   it_behaves_like "get"
   it_behaves_like "list"
+
+  describe "#" do
+    describe "user" do
+      let(:item) { described_class.find(1913) }
+
+      it "returns instances of BunqRb::User" do
+        expect(item.user).to be_instance_of(BunqRb::User)
+      end
+    end
+  end
 end
