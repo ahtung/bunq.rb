cdf556c14c909cec1a91e8d94c8e7611771629c8
Dunya Kirkali <dunyakirkali@gmail.com>
2017-03-18 21:42:14 +0100

    Add validations

--- lib/bunq_rb/objects/monetary_account_bank.rb
+++ lib/bunq_rb/objects/monetary_account_bank.rb
@@ -2,8 +2,11 @@ module BunqRb
   # MonetaryAccountBank
   class MonetaryAccountBank
     include ActiveModel::Model
+    include ActiveModel::Validations
     include BunqRb::Shared
     implements :get, :list, :post, :put
+
+    validates :user_id, presence: true
 
     attr_accessor :id, :created, :updated, :alias, :avatar, :balance, :country, :currency, :daily_limit, :daily_spent, :description, :public_uuid,
       :status, :sub_status, :timezone, :user_id, :monetary_account_profile, :notification_filters, :setting, :overdraft_limit
