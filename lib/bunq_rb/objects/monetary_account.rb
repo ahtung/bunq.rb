5db296285d1034200680db5d0f936e6741c0d9bd
Dunya Kirkali <dunyakirkali@gmail.com>
2017-03-18 20:00:41 +0100

    Implement user for monetary_account and _bank

--- lib/bunq_rb/objects/monetary_account.rb
+++ lib/bunq_rb/objects/monetary_account.rb
@@ -4,13 +4,16 @@ module BunqRb
     include ActiveModel::Model
     include BunqRb::Shared
     implements :get, :list
-    # belongs_to :user
 
     attr_accessor :id, :currency, :created, :updated, :alias, :avatar, :balance, :country, :daily_limit, :daily_spent, :description, :public_uuid,
       :status, :sub_status, :timezone, :user_id, :monetary_account_profile, :notification_filters, :setting, :overdraft_limit
 
     def self.uri
       "/v1/user/#{@user_id}/monetary-account"
+    end
+
+    def user
+      BunqRb::User.find(@user_id)
     end
   end
 end
--- lib/bunq_rb/objects/monetary_account_bank.rb
+++ lib/bunq_rb/objects/monetary_account_bank.rb
@@ -4,13 +4,16 @@ module BunqRb
     include ActiveModel::Model
     include BunqRb::Shared
     implements :get, :list#, :post, :put
-    # belongs_to :user
 
     attr_accessor :id, :created, :updated, :alias, :avatar, :balance, :country, :currency, :daily_limit, :daily_spent, :description, :public_uuid,
       :status, :sub_status, :timezone, :user_id, :monetary_account_profile, :notification_filters, :setting, :overdraft_limit
 
     def self.uri
       "/v1/user/#{@user_id}/monetary-account-bank"
+    end
+
+    def user
+      BunqRb::User.find(@user_id)
     end
   end
 end
