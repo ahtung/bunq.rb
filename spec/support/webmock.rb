27683fcb9d5287ea7e8a68ef70b959863ca8d802
Dunya Kirkali <dunyakirkali@gmail.com>
2017-03-18 14:16:45 +0100

    Boyscout

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -4,41 +4,41 @@ WebMock.disable_net_connect!(allow_localhost: true)
 RSpec.configure do |config|
   config.before(:each) do
     stub_request(:get, "https://sandbox.public.api.bunq.com/v1/user/1913/credential-password-ip/3088/ip").
-      to_return(:status => 200, :body => {
-        "Response": [
+      to_return(status: 200, :body => {
+        "Response" => [
           {
-            "PermittedIp": {
-              "id": 927,
-              "created": "2017-03-18 12:56:56.046564",
-              "updated": "2017-03-18 12:56:56.046564",
-              "ip": "82.68.23.42",
-              "status": "ACTIVE"
+            "PermittedIp" => {
+              "id" => 927,
+              "created" => "2017-03-18 12:56:56.046564",
+              "updated" => "2017-03-18 12:56:56.046564",
+              "ip" => "82.68.23.42",
+              "status" => "ACTIVE"
             }
           }
         ]
       }.to_json, :headers => {})
 
     stub_request(:put, /https:\/\/sandbox.public.api.bunq.com\/v1\/user\/.*\/credential-password-ip\/.*\/ip\/.*/).
-      to_return(:status => 200, :body => {
-        "Response": [
+      to_return(status: 200, :body => {
+        "Response" => [
           {
-            "Id": {
-              "id": 927
+            "Id" => {
+              "id" => 927
             }
           }
         ]
       }.to_json, :headers => {})
     # PermittedIp GET
     stub_request(:get, /https:\/\/sandbox.public.api.bunq.com\/v1\/user\/.*\/credential-password-ip\/.*\/ip\/.*/).
-      to_return(:status => 200, :body => {
-        "Response": [
+      to_return(status: 200, :body => {
+        "Response" => [
           {
-            "PermittedIp": {
-              "id": 927,
-              "created": "2017-03-18 12:56:56.046564",
-              "updated": "2017-03-18 12:56:56.046564",
-              "ip": "82.68.23.42",
-              "status": "ACTIVE"
+            "PermittedIp" => {
+              "id" => 927,
+              "created" => "2017-03-18 12:56:56.046564",
+              "updated" => "2017-03-18 12:56:56.046564",
+              "ip" => "82.68.23.42",
+              "status" => "ACTIVE"
             }
           }
         ]
@@ -46,85 +46,85 @@ RSpec.configure do |config|
 
     # PermittedIp POST
     stub_request(:post, /https:\/\/sandbox.public.api.bunq.com\/v1\/user\/.*\/credential-password-ip\/.*\/ip/).
-      to_return(:status => 200, :body => {
-        "Response": [
+      to_return(status: 200, :body => {
+        "Response" => [
           {
-            "Id": {
-              "id": 927
+            "Id" => {
+              "id" => 927
             }
           }
         ]
       }.to_json, :headers => {})
     # Installation GET
     stub_request(:get, "https://sandbox.public.api.bunq.com/v1/installation/42").
-      to_return(:status => 200, :body => {
-        "Response": [
+      to_return(status: 200, :body => {
+        "Response" => [
           {
-            "Id": {
-              "id": 12
+            "Id" => {
+              "id" => 12
             }
           }
         ]
       }.to_json, :headers => {})
     # Installation LIST
     stub_request(:get, "https://sandbox.public.api.bunq.com/v1/installation").
-      to_return(:status => 200, :body => {
-        "Response": [
+      to_return(status: 200, :body => {
+        "Response" => [
           {
-            "Id": {
-              "id": 12
+            "Id" => {
+              "id" => 12
             }
           }
         ]
       }.to_json, :headers => {})
     stub_request(:post, "https://sandbox.public.api.bunq.com/v1/installation")
       .to_return(status: 200, body: {
-        "Response": [
+        "Response" => [
           {
-            "Id": {
-              "id": 61
+            "Id" => {
+              "id" => 61
             }
           },
           {
-            "Token": {
-              "id": 134,
-              "created": "2015-06-13 23:19:16.215235",
-              "updated": "2015-06-30 09:12:31.981573",
-              "token": "a4f9d888eea84f52722b9baf2f17c289d549edf6e0eccdbf868eb922be306fb6"
+            "Token" => {
+              "id" => 134,
+              "created" => "2015-06-13 23:19:16.215235",
+              "updated" => "2015-06-30 09:12:31.981573",
+              "token" => "a4f9d888eea84f52722b9baf2f17c289d549edf6e0eccdbf868eb922be306fb6"
             }
           },
           {
-            "ServerPublicKey": {
-              "server_public_key": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2cKx+z2NbEapmQWvvov2\nn0k699ZJmWn1yZulOfVeSfHKdGAVj4TlWwAJuvFmThgtHTp+PiJUxKsNUrHHcp+A\nCY0mVH+6f19roBH/B4IS7H5fnXMnpf39IfPDw+hv17bKE+dnuhPuEcloG+LgEOgo\ncjwEb18h5IR3dfbxBHXUce2i4wqfGakAzHumJbPb5XgMMYxng+fqV7uH34CpRpS0\n4bzjuvkwMlRWQsIMUuOvcAjRoCMf1aViFd2+4sEm7RFlyux5PKkq72F\/GITirzlA\nT7T22qrApKnZNPR9y0pGC13FFdx5lVszBNnsKyXDwqrzOsUONSFU+F6JRg6xqUoC\niQIDAQAB\n-----END PUBLIC KEY-----\n"
+            "ServerPublicKey" => {
+              "server_public_key" => "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2cKx+z2NbEapmQWvvov2\nn0k699ZJmWn1yZulOfVeSfHKdGAVj4TlWwAJuvFmThgtHTp+PiJUxKsNUrHHcp+A\nCY0mVH+6f19roBH/B4IS7H5fnXMnpf39IfPDw+hv17bKE+dnuhPuEcloG+LgEOgo\ncjwEb18h5IR3dfbxBHXUce2i4wqfGakAzHumJbPb5XgMMYxng+fqV7uH34CpRpS0\n4bzjuvkwMlRWQsIMUuOvcAjRoCMf1aViFd2+4sEm7RFlyux5PKkq72F\/GITirzlA\nT7T22qrApKnZNPR9y0pGC13FFdx5lVszBNnsKyXDwqrzOsUONSFU+F6JRg6xqUoC\niQIDAQAB\n-----END PUBLIC KEY-----\n"
             }
           }
         ]
       }.to_json)
     stub_request(:post, /https:\/\/sandbox.public.api.bunq.com\/v1\/user\/.*\/monetary-account\/.*\/request-inquiry/).
-      to_return(:status => 200, :body => {
-        "Response": [
+      to_return(status: 200, :body => {
+        "Response" => [
           {
-            "Id": {
-              "id": 7
+            "Id" => {
+              "id" => 7
             }
           }
         ]
       }.to_json, :headers => {})
     stub_request(:get, /https:\/\/sandbox.public.api.bunq.com\/v1\/user\/.*\/monetary-account\/.*\/request-inquiry\/.*/).
-      to_return(:status => 200, :body => {
-        "Response": [
-{
-"RequestInquiry": {
-"id": 42,
-"created": "2015-06-13 23:19:16.215235",
-"updated": "2015-06-30 09:12:31.981573",
-"time_responded": "2015-06-30 09:12:31.981573",
-"time_expiry": "2015-06-30 09:12:31.981573",
-"monetary_account_id": 11,
-"amount_inquired": {
-"value": "12.50",
-"currency": "EUR"
-},
+      to_return(status: 200, :body => {
+        "Response" => [
+          {
+            "RequestInquiry": {
+              "id" => 42,
+              "created": "2015-06-13 23:19:16.215235",
+              "updated": "2015-06-30 09:12:31.981573",
+              "time_responded": "2015-06-30 09:12:31.981573",
+              "time_expiry": "2015-06-30 09:12:31.981573",
+              "monetary_account_id": 11,
+              "amount_inquired": {
+              "value": "12.50",
+              "currency": "EUR"
+            },
 "amount_responded": {
 "value": "12.50",
 "currency": "EUR"
@@ -209,7 +209,7 @@ RSpec.configure do |config|
 "merchant_reference": "ORDER123-BRANCH3-POS4",
 "attachment": [
 {
-"id": 4
+"id" => 4
 }
 ],
 "status": "PENDING",
@@ -246,21 +246,21 @@ RSpec.configure do |config|
 ]
       }.to_json, :headers => {})
     stub_request(:get, /https:\/\/sandbox.public.api.bunq.com\/v1\/installation\/.*\/server-public-key/).
-      to_return(:status => 200, :body => {
-        "Response": [
+      to_return(status: 200, :body => {
+        "Response" => [
           {
-            "ServerPublicKey": {
-              "server_public_key": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2cKx+z2NbEapmQWvvov2\nn0k699ZJmWn1yZulOfVeSfHKdGAVj4TlWwAJuvFmThgtHTp+PiJUxKsNUrHHcp+A\nCY0mVH+6f19roBH/B4IS7H5fnXMnpf39IfPDw+hv17bKE+dnuhPuEcloG+LgEOgo\ncjwEb18h5IR3dfbxBHXUce2i4wqfGakAzHumJbPb5XgMMYxng+fqV7uH34CpRpS0\n4bzjuvkwMlRWQsIMUuOvcAjRoCMf1aViFd2+4sEm7RFlyux5PKkq72F\/GITirzlA\nT7T22qrApKnZNPR9y0pGC13FFdx5lVszBNnsKyXDwqrzOsUONSFU+F6JRg6xqUoC\niQIDAQAB\n-----END PUBLIC KEY-----\n"
+            "ServerPublicKey" => {
+              "server_public_key" => "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2cKx+z2NbEapmQWvvov2\nn0k699ZJmWn1yZulOfVeSfHKdGAVj4TlWwAJuvFmThgtHTp+PiJUxKsNUrHHcp+A\nCY0mVH+6f19roBH/B4IS7H5fnXMnpf39IfPDw+hv17bKE+dnuhPuEcloG+LgEOgo\ncjwEb18h5IR3dfbxBHXUce2i4wqfGakAzHumJbPb5XgMMYxng+fqV7uH34CpRpS0\n4bzjuvkwMlRWQsIMUuOvcAjRoCMf1aViFd2+4sEm7RFlyux5PKkq72F\/GITirzlA\nT7T22qrApKnZNPR9y0pGC13FFdx5lVszBNnsKyXDwqrzOsUONSFU+F6JRg6xqUoC\niQIDAQAB\n-----END PUBLIC KEY-----\n"
             }
           }
         ]
       }.to_json, :headers => {})
     stub_request(:get, "https://sandbox.public.api.bunq.com/v1/user/1913/monetary-account").
-      to_return(:status => 200, :body => {
-        "Response": [
+      to_return(status: 200, :body => {
+        "Response" => [
         {
         "MonetaryAccountBank": {
-        "id": 1933,
+        "id" => 1933,
         "created": "2016-03-15 01:11:13.101139",
         "updated": "2016-03-15 01:11:13.101139",
         "alias": [
@@ -343,33 +343,33 @@ RSpec.configure do |config|
 
       }.to_json, :headers => {})
     stub_request(:post, "https://sandbox.public.api.bunq.com/v1/device-server").
-      to_return(:status => 200, :body => {
+      to_return(status: 200, :body => {
         "Response": [
           {
-            "Id": {
-              "id": 11
+            "Id" => {
+              "id" => 11
             }
           }
         ]
-      }.to_json, :headers => {})
+      }.to_json, headers: {})
 
     stub_request(:post, "https://sandbox.public.api.bunq.com/v1/session-server").
-      to_return(:status => 200, :body => {
+      to_return(status: 200, :body => {
 "Response": [
 {
-"Id": {
-"id": 16
+"Id" => {
+"id" => 16
 }
 },
 {
 "Token": {
-"id": 839,
+"id" => 839,
 "token": "b165cce82bbd229b55962f90b4efedd706b3f616f0de831547ff62262f2924e3"
 }
 },
 {
 "UserCompany": {
-"id": 42,
+"id" => 42,
 "created": "2015-06-13 23:19:16.215235",
 "updated": "2015-06-30 09:12:31.981573",
 "public_uuid": "252e-fb1e-04b74214-b9e9467c3-c6d2fbf",
@@ -413,53 +413,53 @@ RSpec.configure do |config|
 "po_box": "09392",
 "postal_code": "1234AA",
 "city": "Amsterdam",
-"country": "NL"
-},
-"version_terms_of_service": "1.2",
-"director_alias": {
-"uuid": "252e-fb1e-04b74214-b9e9467c3-c6d2fbf",
-"avatar": {
-"uuid": "5a442bed-3d43-4a85-b532-dbb251052f4a",
-"anchor_uuid": "f0de919f-8c36-46ee-acb7-ea9c35c1b231",
-"image": [
-{
-"attachment_public_uuid": "d93e07e3-d420-45e5-8684-fc0c09a63686",
-"content_type": "image/jpeg",
-"height": 380,
-"width": 520
-}
-]
-},
-"public_nick_name": "Mary",
-"display_name": "Mary",
-"country": "NL"
-},
-"language": "en_US",
-"region": "en_US",
-"ubo": [
-{
-"name": "A. Person",
-"date_of_birth": "1990-03-27",
-"nationality": "NL"
-}
-],
-"status": "ACTIVE",
-"sub_status": "APPROVAL",
-"session_timeout": 1,
-"daily_limit_without_confirmation_login": {
-"value": "12.50",
-"currency": "EUR"
-},
-"notification_filters": [
-{
-"notification_delivery_method": "URL",
-"notification_target": "https://my.company.com/callback-url",
-"category": "PAYMENT"
-}
-]
-}
-}
-]
+        "country": "NL"
+        },
+        "version_terms_of_service": "1.2",
+        "director_alias": {
+        "uuid": "252e-fb1e-04b74214-b9e9467c3-c6d2fbf",
+        "avatar": {
+        "uuid": "5a442bed-3d43-4a85-b532-dbb251052f4a",
+        "anchor_uuid": "f0de919f-8c36-46ee-acb7-ea9c35c1b231",
+        "image": [
+        {
+        "attachment_public_uuid": "d93e07e3-d420-45e5-8684-fc0c09a63686",
+        "content_type": "image/jpeg",
+        "height": 380,
+        "width": 520
+        }
+        ]
+        },
+        "public_nick_name": "Mary",
+        "display_name": "Mary",
+        "country": "NL"
+        },
+        "language": "en_US",
+        "region": "en_US",
+        "ubo": [
+        {
+        "name": "A. Person",
+        "date_of_birth": "1990-03-27",
+        "nationality": "NL"
+        }
+              ],
+              "status": "ACTIVE",
+              "sub_status": "APPROVAL",
+              "session_timeout": 1,
+              "daily_limit_without_confirmation_login": {
+              "value": "12.50",
+              "currency": "EUR"
+            },
+            "notification_filters": [
+              {
+                "notification_delivery_method": "URL",
+                "notification_target": "https://my.company.com/callback-url",
+                "category": "PAYMENT"
+              }
+            ]
+          }
+        }
+      ]
     }.to_json, headers: {})
   end
 
