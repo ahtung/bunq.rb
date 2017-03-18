0e09c2357f559b962483351ecd72813130afb697
Dunya Kirkali <dunyakirkali@gmail.com>
2017-03-18 14:12:05 +0100

    Switch to webmock

--- spec/bunq_rb/objects/permitted_ip_spec.rb
+++ spec/bunq_rb/objects/permitted_ip_spec.rb
@@ -20,16 +20,16 @@ RSpec.describe BunqRb::PermittedIp, active_session: true do
 
   describe "PUT /v1/user/:user_id/credential-password-ip/:credential_password_id/ip/:id" do
     it "returns PermittedIp hash" do
-      permitted_ip = described_class.find(877, user_id, credential_password_id)
-      response = permitted_ip.update({ status: 'INACTIVE' }, user_id, credential_password_id)
-      expect(response["id"]).to eq(877)
+      permitted_ip = described_class.find(927, user_id, credential_password_id)
+      response = permitted_ip.update({ status: "INACTIVE" }, user_id, credential_password_id)
+      expect(response["id"]).to eq(927)
     end
   end
 
   describe "POST /v1/user/:user_id/credential-password-ip/:credential_password_id/ip" do
     it "returns PermittedIp hash" do
-      response = described_class.create({ ip: "84.168.43.29", status: 'INACTIVE' }, user_id, credential_password_id)
-      expect(response["id"]).to eq(925)
+      response = described_class.create({ ip: "84.168.43.29", status: "INACTIVE" }, user_id, credential_password_id)
+      expect(response["id"]).not_to be_nil
     end
   end
 end
