7cfb38cf3c20b0e0b6cd679ba1143d309aca1704
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-03-14 09:08:02 +0100

    Adapt tests

--- spec/bunq_rb/client_spec.rb
+++ spec/bunq_rb/client_spec.rb
@@ -1,10 +1,6 @@
 require "spec_helper"
 
 describe BunqRb::Client do
-  it "has a BASE_URL" do
-    expect(described_class::BASE_URL).not_to be nil
-  end
-
   describe "." do
     describe "connection" do
       it "returns a Faraday::Connection object" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ RSpec.configure do |config|
     BunqRb.configure do |bunqrb_config|
       bunqrb_config.api_key = "c08bbdb62e1d1795ae7e933bc833452fda9c317b4b9d717baeabbc17f8190df9"
       bunqrb_config.key = OpenSSL::PKey::RSA.new 2048
+      bunqrb_config.url = "https://sandbox.public.api.bunq.com"
     end
 
     @installation, @token, @server_public_key = BunqRb::Installation.create(
