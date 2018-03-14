23cc6d320ffb2a25d6aed184094a1cdb567585d2
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-03-14 22:56:36 +0100

    Fix hardcoded user_id in monetary_account

--- lib/bunq_rb/objects/monetary_account.rb
+++ lib/bunq_rb/objects/monetary_account.rb
@@ -1,19 +1,20 @@
 module BunqRb
   # MonetaryAccount
   class MonetaryAccount
-    attr_reader :id, :currency
+    attr_reader :id, :currency, :description
 
     def initialize(hsh = {})
       @id = hsh["id"]
       @currency = hsh["currency"]
+      @description = hsh["description"]
     end
 
     def self.url(user_id)
       "/v1/user/#{user_id}/monetary-account"
     end
 
-    def self.all
-      response = Client.send_method(:get, url(1913))
+    def self.all(user_id)
+      response = Client.send_method(:get, url(user_id))
       response.map { |resp| new(resp["MonetaryAccountBank"]) }
     end
   end
