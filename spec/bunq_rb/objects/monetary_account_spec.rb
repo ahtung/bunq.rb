878847c84fa0ca71a7bbd67275bb04d2d1ef5328
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-03-14 22:59:21 +0100

    Adapt spec

--- spec/bunq_rb/objects/monetary_account_spec.rb
+++ spec/bunq_rb/objects/monetary_account_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 RSpec.describe BunqRb::MonetaryAccount do
   describe "GET /v1/user/1913/monetary-account", active_session: true do
     it "returns an array of :id's" do
-      installations = described_class.all
+      installations = described_class.all(1913)
       expect(installations.count).to be(1)
     end
   end
