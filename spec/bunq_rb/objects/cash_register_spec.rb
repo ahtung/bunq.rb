2a0c5b56bd61388825441936bac454670f81ca58
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-03-20 08:52:32 +0100

    Add cash_register specs

---
+++ spec/bunq_rb/objects/cash_register_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+describe BunqRb::CashRegister, active_session: true do
+  describe "POST /v1/user/:user_id/monetary-account/:monetary_account_id/cash-register" do
+    it "returns a BunqRb::CashRegister" do
+      user_id = 1913
+      monetary_account_id = 11
+      response = described_class.create({
+        name: "Kassa 12",
+        status: "PENDING_APPROVAL",
+        avatar_uuid: "5a442bed-3d43-4a85-b532-dbb251052f4a"
+      }, user_id, monetary_account_id)
+      expect(response["id"]).not_to be_nil
+    end
+  end
+end
