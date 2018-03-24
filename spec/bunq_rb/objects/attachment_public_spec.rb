c98f00d64d732819a9aececc798c92e947f40686
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-03-24 10:26:11 +0100

    Clean up specs

--- spec/bunq_rb/objects/attachment_public_spec.rb
+++ spec/bunq_rb/objects/attachment_public_spec.rb
@@ -1,17 +1,6 @@
 require "spec_helper"
 
-RSpec.describe BunqRb::AttachmentPublic do
-  describe "POST /v1/attachment-public", active_session: true do
-    it "returns an :uuid" do
-      attachment_public = described_class.create({})
-      expect(attachment_public.uuid).to eq("5a442b1d-3d43-4285-b532-dbb21055824a")
-    end
-  end
-
-  describe "GET /v1/attachment-public/:id", active_session: true do
-    it "returns an :id" do
-      attachment_public = described_class.find("b07faaa7-003a-4cdf-a2c1-434e71d42fca")
-      expect(attachment_public.uuid).to eq("b07faaa7-003a-4cdf-a2c1-434e71d42fca")
-    end
-  end
+RSpec.describe BunqRb::AttachmentPublic, active_session: true do
+  it_behaves_like "get", ["b07faaa7-003a-4cdf-a2c1-434e71d42fca"]
+  it_behaves_like "post", {}
 end
--- spec/bunq_rb/objects/installation_server_public_key_spec.rb
+++ spec/bunq_rb/objects/installation_server_public_key_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
-RSpec.describe BunqRb::InstallationServerPublicKey do
-  describe "GET /v1/installation/:installation_id/server-public-key", active_session: true do
+RSpec.describe BunqRb::InstallationServerPublicKey, active_session: true do
+  describe "GET /v1/installation/:installation_id/server-public-key" do
     let(:server_public_key) { described_class.new(installation_id: @installation.id) }
 
     it "returns a server_public_key" do
